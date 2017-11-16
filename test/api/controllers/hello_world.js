09e022c3ca655b3eac193d650e0ed656f2337b6c
Eric Takahashi <erictt@br.ibm.com>
2017-11-16 09:59:49 +0000

    Change test scenario.

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql({ name: 'Hello, stranger!' });
+            res.body.should.eql({ name: 'Hello there, stranger!' });
 
             done();
           });
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql({ name: 'Hello, Scott!' });
+            res.body.should.eql({ name: 'Hello there, Scott!' });
 
             done();
           });
